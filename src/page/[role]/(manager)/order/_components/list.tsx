5fefe20e07413b11c82e9c419a073f864e48f589
vmhfpt <vuminhhungltt904@gmail.com>
2024-06-29 23:09:16 +0700

    abc

--- src/page/[role]/(manager)/order/_components/list.tsx
+++ src/page/[role]/(manager)/order/_components/list.tsx
@@ -20,6 +20,7 @@ export default function ListOrder() {
     (async() => {
         const response = await axios.get('http://localhost:3000/orders');
         setDataItem(response.data);
+        // console
     } )();
   }, [])
  
