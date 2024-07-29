6db63060547c99f4aae6eda722a26321ec3ab76e
DangDat2 <DangDat2>
2024-07-29 22:05:35 +0700

    Update profile

--- src/page/[role]/(base)/AccountPage/AccountPage.tsx
+++ src/page/[role]/(base)/AccountPage/AccountPage.tsx
@@ -1,17 +1,157 @@
 import Label from "../components/Label/Label";
-import  { FC } from "react";
+import  { FC, useEffect, useState } from "react";
 import ButtonPrimary from "../shared/Button/ButtonPrimary";
-import Input from "../shared/Input/Input";
-import Select from "../shared/Select/Select";
-import Textarea from "../shared/Textarea/Textarea";
+import { Form, Input, Upload} from 'antd'
 import { Helmet } from "react-helmet-async";
 import { avatarImgs } from "../../../../contains/fakeData";
+import {Select as SelectAntd } from 'antd';
+import type { SelectProps  } from 'antd';
+import { useNavigate } from "react-router-dom";
+import LoadingUser from "../../(manager)/user/util/Loading";
+import ErrorLoad from "../../(manager)/components/util/ErrorLoad";
+import { useGetProvincesQuery, useLazyGetDistrictsQuery } from "@/utils/addressRTKQuery";
+import { popupError, popupSuccess } from "../../shared/Toast";
+import { Iuser } from "@/common/types/user.interface";
+import { useGetUserQuery, useUpdateUserMutation } from "../../(manager)/user/UsersEndpoints";
 
 export interface AccountPageProps {
   className?: string;
 }
 
 const AccountPage: FC<AccountPageProps> = ({ className = "" }) => {
+  const layout = {
+    labelCol: { span: 8 },
+    wrapperCol: { span: 16 }
+  }
+  
+  const validateMessages = {
+    required: '${label} is required!',
+    types: {
+      email: '${label} is not a valid email!',
+      number: '${label} is not a valid number!'
+    },
+    number: {
+      range: '${label} must be between ${min} and ${max}'
+    },
+    
+  }
+  const [file, setFile] = useState({
+    data: {},
+    loading: false
+  })
+  const handleUpload = async (options: any) => {
+    const { onSuccess, file } = options
+    setFile({
+      data: file,
+      loading: false
+    })
+    onSuccess('Upload successful', file)
+ 
+  }
+  const user = JSON.parse(String(localStorage.getItem('user')));
+  const {data : dataItem, isLoading : dataLoading } = useGetUserQuery(user?.id);
+ 
+  const [updateUser, { isLoading : loadingUpdateUser }] = useUpdateUserMutation();
+  const [form] = Form.useForm();
+  const [optionsDistrict, setOptionDistrict] = useState<SelectProps['options']>([])
+  
+  const onFinish = async (values: Iuser | any) => {
+    console.log(values);
+    
+    const formData = new FormData()
+    for (const key  in values ) {
+       if(String(key) == 'upload'){
+        if( values[key]){
+          formData.append('image',values[key][0].originFileObj);
+        }
+       
+        continue;
+       }
+       if(String(key) == 'is_active'){
+        if(values[key]){
+           formData.append(key,'1')
+        }else {
+           formData.append(key,'0')
+        }
+        continue;
+       }
+       formData.append(key,values[key])
+       
+    }
+    formData.append('is_virtual','0');
+    try {
+      const payload = {
+        id : user.id,
+        data : formData
+      }
+      await updateUser(payload).unwrap();
+      popupSuccess('Update user success');
+      handleCancel();
+    } catch (error) {
+      popupError('Update user error');
+    }
+    
+  }
+
+  const [getDistrict, { data : dataDistricts , isLoading : districtLoading}] = useLazyGetDistrictsQuery();
+
+
+
+
+  useEffect(() => {
+    setOptionDistrict(() => {
+          return dataDistricts?.data.map((item : {id : number, name : string}) => {
+               return {
+                value : `${item.name}`,
+                label : item.name
+              }
+           });
+    })
+ }, [dataDistricts])
+
+
+  const options: SelectProps['options'] = [];
+  
+
+  const {
+    data : provinces,
+    isLoading,
+    isError
+  } = useGetProvincesQuery({});
+
+  
+   
+  provinces?.data.forEach((item : {id : number, name : string}) => {
+    options.push({
+      value : `${item.name}-${item.id}`,
+      label : item.name
+     })
+  });
+
+
+  const onChangeProvince = async (value : string) => {
+    form.resetFields(['district']);
+    if(value){
+      const splitStr = value.split(/-(\d+)/);
+      const provinceId = splitStr[1];
+      
+      await getDistrict(provinceId);
+     
+      
+    }else {
+       setOptionDistrict([]);
+    }
+    
+
+}
+  const navigate = useNavigate()
+
+  const handleCancel = () => {
+    navigate('..')
+  }
+
+  if(isLoading || dataLoading) return <LoadingUser />
+  if(isError) return <ErrorLoad />
   return (
     <div className={`nc-AccountPage ${className}`} data-nc-id="AccountPage">
       <Helmet>
@@ -22,12 +162,21 @@ const AccountPage: FC<AccountPageProps> = ({ className = "" }) => {
           <h2 className="text-2xl sm:text-3xl font-semibold">
             Account infomation
           </h2>
+          <Form
+            initialValues={dataItem?.data}
+            form={form}
+            {...layout}
+            name='nest-messages'
+            onFinish={onFinish}
+            style={{ maxWidth: 600 }}
+            validateMessages={validateMessages}
+          >
           <div className="flex flex-col md:flex-row">
             <div className="flex-shrink-0 flex items-start">
               {/* AVATAR */}
               <div className="relative rounded-full overflow-hidden flex">
                 <img
-                  src={avatarImgs[2]}
+                  src={dataItem?.data.image}
                   alt=""
                   className="w-32 h-32 rounded-full object-cover z-0"
                 />
@@ -47,19 +196,26 @@ const AccountPage: FC<AccountPageProps> = ({ className = "" }) => {
                       strokeLinejoin="round"
                     />
                   </svg>
-
-                  <span className="mt-1 text-xs">Change Image</span>
+                  <Form.Item
+                  name='upload'
+                  label='.'
+                  valuePropName='fileList'
+                  getValueFromEvent={(e) => (Array.isArray(e) ? e : e && e.fileList)}
+                >
+                  <Upload name='image' listType='picture' customRequest={handleUpload}>
+                    <span className="mt-1 text-xs text-white">Change Image</span>
+                  </Upload>
+                </Form.Item>
                 </div>
-                <input
-                  type="file"
-                  className="absolute inset-0 opacity-0 cursor-pointer"
-                />
+                
               </div>
             </div>
             <div className="flex-grow mt-10 md:mt-0 md:pl-16 max-w-3xl space-y-6">
               <div>
-                <Label>Full name</Label>
-                <Input className="mt-1.5" defaultValue="Enrico Cole" />
+                <Label>User name</Label>
+                <Form.Item name="username" rules={[{required: true }]}>
+                  <Input name="username" className="mt-1.5 !rounded-l-none w-[445px]" defaultValue="Enrico Cole" />
+                </Form.Item>
               </div>
 
               {/* ---- */}
@@ -68,18 +224,21 @@ const AccountPage: FC<AccountPageProps> = ({ className = "" }) => {
               <div>
                 <Label>Email</Label>
                 <div className="mt-1.5 flex">
-                  <span className="inline-flex items-center px-2.5 rounded-l-2xl border border-r-0 border-neutral-200 dark:border-neutral-700 bg-neutral-50 dark:bg-neutral-800 text-neutral-500 dark:text-neutral-400 text-sm">
+                  <span className="h-[40px] inline-flex items-center px-2.5 rounded-l-2xl border border-r-0 border-neutral-200 dark:border-neutral-700 bg-neutral-50 dark:bg-neutral-800 text-neutral-500 dark:text-neutral-400 text-sm">
                     <i className="text-2xl las la-envelope"></i>
                   </span>
-                  <Input
-                    className="!rounded-l-none"
-                    placeholder="example@email.com"
-                  />
+                  <Form.Item name="email">
+                    <Input
+                      name="email"
+                      className="!rounded-l-none w-[400px]"
+                      placeholder="example@email.com"
+                    />
+                  </Form.Item>
                 </div>
               </div>
 
               {/* ---- */}
-              <div className="max-w-lg">
+              {/* <div className="max-w-lg">
                 <Label>Date of birth</Label>
                 <div className="mt-1.5 flex">
                   <span className="inline-flex items-center px-2.5 rounded-l-2xl border border-r-0 border-neutral-200 dark:border-neutral-700 bg-neutral-50 dark:bg-neutral-800 text-neutral-500 dark:text-neutral-400 text-sm">
@@ -91,54 +250,106 @@ const AccountPage: FC<AccountPageProps> = ({ className = "" }) => {
                     defaultValue="1990-07-22"
                   />
                 </div>
-              </div>
+              </div> */}
               {/* ---- */}
               <div>
                 <Label>Addess</Label>
                 <div className="mt-1.5 flex">
-                  <span className="inline-flex items-center px-2.5 rounded-l-2xl border border-r-0 border-neutral-200 dark:border-neutral-700 bg-neutral-50 dark:bg-neutral-800 text-neutral-500 dark:text-neutral-400 text-sm">
+                  <span className="h-[40px] inline-flex items-center px-2.5 rounded-l-2xl border border-r-0 border-neutral-200 dark:border-neutral-700 bg-neutral-50 dark:bg-neutral-800 text-neutral-500 dark:text-neutral-400 text-sm">
                     <i className="text-2xl las la-map-signs"></i>
                   </span>
-                  <Input
-                    className="!rounded-l-none"
-                    defaultValue="New york, USA"
-                  />
+                  <Form.Item name="address">
+                    <Input
+                      name="address"
+                      className="!rounded-l-none w-[400px]"
+                      defaultValue="New york, USA"
+                    />
+                  </Form.Item>
                 </div>
               </div>
 
-              {/* ---- */}
               <div>
+                <Label>County</Label>
+                <Form.Item name="county" rules={[{required: true }]}>
+                  <SelectAntd
+                
+                      style={{ width: '100%' }}
+                    
+                      options={[
+                    
+                        { value: 'Việt Nam', label: 'VietNam' },
+                      
+                      ]}
+                    />
+                </Form.Item>
+              </div>         
+
+              <div>
+                <Label>City</Label>
+                <Form.Item name="city" rules={[{required: true }]}>
+                  <SelectAntd
+                      
+                      style={{ width: '100%' }}
+                      placeholder="Enter name province"
+                      options={options}
+                      onChange={(value) => onChangeProvince(value)}
+                    />
+                </Form.Item>
+              </div>
+
+              <div>
+                <Label>District</Label>
+                <Form.Item name="district" rules={[{required: true }]}>
+                      <SelectAntd
+                          loading={districtLoading}                      
+                          style={{ width: '100%' }}
+                          placeholder="Enter name district"
+                          options={optionsDistrict}
+                        />
+                </Form.Item>
+              </div>
+
+              {/* ---- */}
+              {/* <div>
                 <Label>Gender</Label>
                 <Select className="mt-1.5">
                   <option value="Male">Male</option>
                   <option value="Female">Female</option>
                   <option value="Other">Other</option>
                 </Select>
-              </div>
+              </div> */}
 
               {/* ---- */}
               <div>
                 <Label>Phone number</Label>
                 <div className="mt-1.5 flex">
-                  <span className="inline-flex items-center px-2.5 rounded-l-2xl border border-r-0 border-neutral-200 dark:border-neutral-700 bg-neutral-50 dark:bg-neutral-800 text-neutral-500 dark:text-neutral-400 text-sm">
-                    <i className="text-2xl las la-phone-volume"></i>
+                  <span className="h-[40px] inline-flex items-center px-2.5 rounded-l-2xl border border-r-0 border-neutral-200 dark:border-neutral-700 bg-neutral-50 dark:bg-neutral-800 text-neutral-500 dark:text-neutral-400 text-sm">
+                    <i className="text-xl las la-phone-volume"></i>
                   </span>
-                  <Input
-                    className="!rounded-l-none"
-                    defaultValue="003 888 232"
-                  />
+                  <Form.Item name="phone" rules={[{required: true }]}>
+                    <Input
+                      name="phone"
+                      className="!rounded-l-none w-[400px]"
+                      defaultValue="003 888 232"
+                    />
+                  </Form.Item>
                 </div>
               </div>
               {/* ---- */}
-              <div>
+              {/* <div>
                 <Label>About you</Label>
                 <Textarea className="mt-1.5" defaultValue="..." />
-              </div>
+              </div> */}
               <div className="pt-2">
-                <ButtonPrimary>Update account</ButtonPrimary>
+                <Form.Item className='mt-3' wrapperCol={{ ...layout.wrapperCol, offset: 8 }}>
+                  <ButtonPrimary loading={loadingUpdateUser || file.loading} disabled={loadingUpdateUser || file.loading}>
+                    Update account
+                  </ButtonPrimary>
+                </Form.Item>
               </div>
             </div>
           </div>
+          </Form>
         </div>
     </div>
   );
